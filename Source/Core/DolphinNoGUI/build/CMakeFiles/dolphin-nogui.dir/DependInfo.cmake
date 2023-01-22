f1c4b71cda3c2c19b75f70608b859b0b8d3f993f
GhostRain <zayd@qumsieh.net>
2023-01-21 18:32:28 -0800

    force usage of interpreter

---
+++ Source/Core/DolphinNoGUI/build/CMakeFiles/dolphin-nogui.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/zaydq/Documents/dolphin/Source/Core/DolphinNoGUI/MainNoGUI.cpp" "CMakeFiles/dolphin-nogui.dir/MainNoGUI.o" "gcc" "CMakeFiles/dolphin-nogui.dir/MainNoGUI.o.d"
+  "/home/zaydq/Documents/dolphin/Source/Core/DolphinNoGUI/Platform.cpp" "CMakeFiles/dolphin-nogui.dir/Platform.o" "gcc" "CMakeFiles/dolphin-nogui.dir/Platform.o.d"
+  "/home/zaydq/Documents/dolphin/Source/Core/DolphinNoGUI/PlatformFBDev.cpp" "CMakeFiles/dolphin-nogui.dir/PlatformFBDev.o" "gcc" "CMakeFiles/dolphin-nogui.dir/PlatformFBDev.o.d"
+  "/home/zaydq/Documents/dolphin/Source/Core/DolphinNoGUI/PlatformHeadless.cpp" "CMakeFiles/dolphin-nogui.dir/PlatformHeadless.o" "gcc" "CMakeFiles/dolphin-nogui.dir/PlatformHeadless.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Source/Core/DolphinNoGUI/build/CMakeFiles/dolphin-nogui.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dolphin-nogui.dir/MainNoGUI.o"
+  "CMakeFiles/dolphin-nogui.dir/MainNoGUI.o.d"
+  "CMakeFiles/dolphin-nogui.dir/Platform.o"
+  "CMakeFiles/dolphin-nogui.dir/Platform.o.d"
+  "CMakeFiles/dolphin-nogui.dir/PlatformFBDev.o"
+  "CMakeFiles/dolphin-nogui.dir/PlatformFBDev.o.d"
+  "CMakeFiles/dolphin-nogui.dir/PlatformHeadless.o"
+  "CMakeFiles/dolphin-nogui.dir/PlatformHeadless.o.d"
+  "dolphin-emu-nogui"
+  "dolphin-emu-nogui.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dolphin-nogui.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
